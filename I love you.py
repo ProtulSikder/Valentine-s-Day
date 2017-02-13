7e82fa2cb55ef3b4e1aa8663b728f085b759b0e8
ProtulSikder <protul.51@gmail.com>
2017-02-14 00:47:16 +0600

    Add files via upload

---
+++ I love you.py
@@ -0,0 +1,30 @@
+'''
+A fun program in valentines day
+that says 'I love you!'
+by: Nasir Sikder Protul.
+
+In this program I used
+gTTS module to convert text into mp3
+and then used pygame.mixer.music to
+read the mp3 file.
+
+The particular reason I used gTTS
+is because it's a lady voice :D :D
+'''
+
+from gtts import gTTS
+from pygame import mixer
+
+#Specify the language and put it in a variable
+tts = gTTS(text = 'I love you', lang = 'en')
+
+#Save the file as an mp3 file
+tts.save('love.mp3')
+
+mixer.init()
+
+#Load the mp3 file
+mixer.music.load('love.mp3')
+
+#Play the file
+mixer.music.play()
